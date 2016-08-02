b286ce2c3207ba0a06dd7dde5321c878dbed80ad
Owen Williams <hi@owened.co.nz>
2016-08-03 00:15:39 +0200

    Photography pages

---
+++ src/_photography/amsterdam.md
@@ -0,0 +1,11 @@
+---
+title: A few days around Amsterdam.
+location: Amsterdam, The Netherlands
+collection: photography
+date: June 2016
+layout: gallery
+---
+
+It's been a while since I posted any meaningful photos here, so I thought it was about time I got onto it.
+
+{% include gallery-layout.html gallery=site.data.amsterdam %}
--- src/assets/javascript/main.js
+++ src/assets/javascript/main.js
@@ -17,30 +17,6 @@ function getTracks(){
 
 getTracks();
 
-function funFact() {
-  var facts = ["My favorite weekend activity is hiking","I'm a little obsessed with bikes"];
-}
-
-jQuery(document).ready(function($) {
-
-    var siteUrl = 'http://'+(document.location.hostname||document.location.host);
-
-    $(document).delegate('a[href^="/"],a[href^="'+siteUrl+'"]', "click", function(e) {
-        e.preventDefault();
-        History.pushState({}, "", this.pathname);
-    });
-
-    History.Adapter.bind(window, 'statechange', function(){
-        var State = History.getState();
-        $.get(State.url, function(data){
-            document.title = $(data).find("title").text();
-            $('.content').html($(data).find('.content'));
-            _gaq.push(['_trackPageview', State.url]);
-        });
-    });
-
-});
-
 function shrink() {
     $('.hero').fadeOut(function(){
 
@@ -51,6 +27,4 @@ function shrink() {
 
     });
     $('.right-box').addClass('fullSize');
-
-
 }
